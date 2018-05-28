abec547eb6d1754d9d4959d9b5208e501feef7a0
Sudhanshu Kumar Singh <sudhanshukumarsingh@Sudhanshus-MacBook-Air.local>
2018-05-28 21:52:42 +0530

    added improved permission to user profile feed items api

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -11,4 +11,13 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
-        
+
+class PostOwnStatus(permissions.BasePermission):
+    """Allow User To Update There Own Status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check The User Trying To Update Theire Own Status."""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile.id == request.user.id
